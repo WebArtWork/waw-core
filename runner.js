d9e7ce3f8d8189e0b9fa73f459316418bfd6b998
Honchar Denys <crackeraki@gmail.com>
2020-02-02 14:27:16 +0200

    added basic structure and use of package

--- runner.js
+++ runner.js
@@ -1,8 +1,29 @@
-module.exports.new = function(params) {
-	console.log(params);
-	console.log('something happened');
+/*
+*	Create new project
+*/
+const new_project = function(params) {
+	console.log('Create new project', params);
 	process.exit(1);
 };
-module.exports.add = function(params) {
-	
+module.exports.new = new_project;
+module.exports.n = new_project;
+/*
+*	Create new part
+*/
+const add = function(params) {
+	console.log('Create new part', params);
+	process.exit(1);
 };
+module.exports.add = add;
+module.exports.a = add;
+/*
+*	Version Management
+*/
+const version = function(params){
+	console.log('waw: 1.0.0', params);
+	process.exit(1);
+}
+module.exports['--version'] = version;
+module.exports['-v'] = version;
+module.exports['version'] = version;
+module.exports['v'] = version;
