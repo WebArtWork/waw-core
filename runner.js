c6987ef59596388dbe344b2649d80fcbfffd877d
Honchar Denys <crackeraki@gmail.com>
2022-01-23 13:50:05 +0200

    fixed runner based on latest waw

--- runner.js
+++ runner.js
@@ -3,24 +3,6 @@ const readline = require('readline').createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
-
-const isDirectory = source => fs.lstatSync(source).isDirectory();
-const getDirectories = source => {
-	if (!fs.existsSync(source)) {
-		return [];
-	}
-	return fs.readdirSync(source).map(name => require('path').join(source, name)).filter(isDirectory);
-}
-const git_fetch = function(git, location, repo, branch='master', cb = ()=>{}){
-	let git_repo = git(location);
-	git_repo.init(function(){
-		git_repo.addRemote('origin', repo, function(err){
-			git_repo.fetch('--all', function(err){
-				git_repo.reset('origin/'+branch, cb);
-			});
-		});
-	});
-}
 /*
 *	Create new project
 */
@@ -30,15 +12,15 @@ const git_fetch = function(git, location, repo, branch='master', cb = ()=>{}){
 		'3) waw Server': 'https://github.com/WebArtWork/wawServer.git',
 		'4) waw Server + Angular + Template': 'https://github.com/WebArtWork/wawNgxPlatform.git',
 	};
-	const new_project = function(params) {
-		if(!params.new_project) params.new_project={};
-		if(!params.new_project.name){
-			if(params.argv.length){
-				if (fs.existsSync(process.cwd()+'/'+params.argv[0])) {
+	const new_project = function(waw) {
+		if(!waw.new_project) waw.new_project={};
+		if(!waw.new_project.name){
+			if(waw.argv.length){
+				if (fs.existsSync(process.cwd()+'/'+waw.argv[0])) {
 					console.log('This project already exists in current directory');
 					process.exit(0);
 				}else{
-					params.new_project.name = params.argv[0];
+					waw.new_project.name = waw.argv[0];
 				}
 			}else{
 				return readline.question('Provide name for the project you want to create: ', function(answer){
@@ -46,18 +28,18 @@ const git_fetch = function(git, location, repo, branch='master', cb = ()=>{}){
 						if (fs.existsSync(process.cwd()+'/'+answer)) {
 							console.log('This project already exists in current directory');
 						}else{
-							params.new_project.name = answer;
+							waw.new_project.name = answer;
 						}
 					}else{
 						console.log('Please type your project name');
 					}
-					new_project(params);
+					new_project(waw);
 				});
 			}
 		}
-		if(!params.new_project.repo){
-			if(params.argv.length>1){
-				params.new_project.repo = params.argv[1];
+		if(!waw.new_project.repo){
+			if(waw.argv.length>1){
+				waw.new_project.repo = waw.argv[1];
 			}else{
 				let text = 'Which project you want to start with?', counter=0, repos={};
 				for(let key in list){
@@ -67,20 +49,20 @@ const git_fetch = function(git, location, repo, branch='master', cb = ()=>{}){
 				text += '\nChoose number: ';
 				return readline.question(text, function(answer){
 					if(!answer||!repos[parseInt(answer)]) return new_project();
-					params.new_project.repo = repos[parseInt(answer)];
-					new_project(params);
+					waw.new_project.repo = repos[parseInt(answer)];
+					new_project(waw);
 				});
 			}
 		}
-		let folder = process.cwd()+'/'+params.new_project.name;
+		let folder = process.cwd()+'/'+waw.new_project.name;
 		fs.mkdirSync(folder, { recursive: true });
-		let repo = params.git(folder);
+		let repo = waw.git(folder);
 		repo.init(function(){
-			repo.addRemote('origin', params.new_project.repo, function(err){
+			repo.addRemote('origin', waw.new_project.repo, function(err){
 				repo.fetch('--all', function(err){
 					let branch = 'master';
-					if(params.argv.length>2){
-						branch = params.argv[2];
+					if(waw.argv.length>2){
+						branch = waw.argv[2];
 					}
 					repo.reset('origin/'+branch, err=>{
 						fs.rmdirSync(folder+'/.git', { recursive: true });
@@ -94,21 +76,21 @@ const git_fetch = function(git, location, repo, branch='master', cb = ()=>{}){
 	module.exports.new = new_project;
 	module.exports.n = new_project;
 /*
-*	Create new part
+*	Create new module
 */
-	const new_part = function(params) {
+	const new_module = function(waw) {
 		if (!fs.existsSync(process.cwd()+'/config.json')) {
 			console.log('You are located not in waw project');
 			process.exit(0);
 		}
-		if(!params.new_part) params.new_part={};
-		if(!params.new_part.name){
-			if(params.argv.length){
-				if (fs.existsSync(process.cwd()+'/server/'+params.argv[0].toLowerCase())) {
+		if(!waw.new_module) waw.new_module={};
+		if(!waw.new_module.name){
+			if(waw.argv.length){
+				if (fs.existsSync(process.cwd()+'/server/'+waw.argv[0].toLowerCase())) {
 					console.log('This module already exists in current project');
 					process.exit(0);
 				}else{
-					params.new_part.name = params.argv[0];
+					waw.new_module.name = waw.argv[0];
 				}
 			}else{
 				return readline.question('Provide name for the module you want to create: ', function(answer){
@@ -116,54 +98,43 @@ const git_fetch = function(git, location, repo, branch='master', cb = ()=>{}){
 						if (fs.existsSync(process.cwd()+'/'+answer.toLowerCase())) {
 							console.log('This module already exists in current project');
 						}else{
-							params.new_part.name = answer;
+							waw.new_module.name = answer;
 						}
 					}else{
 						console.log('Please type your project name');
 					}
-					new_part(params);
+					new_module(waw);
 				});
 			}
 		}
-		let folder = process.cwd()+'/server/'+params.new_project.name;
-		if(params.argv.length > 1){
+		let folder = process.cwd()+'/server/'+waw.new_project.name;
+		if(waw.argv.length > 1){
 			fs.mkdirSync(folder, { recursive: true });
-			let repo = params.git(folder);
-			repo.init(function(){
-				repo.addRemote('origin', params.argv[1], function(err){
-					repo.fetch('--all', function(err){
-						let branch = 'master';
-						if(params.argv.length>2){
-							branch = params.argv[2];
-						}
-						repo.reset('origin/'+branch, err=>{
-							console.log('Module has been created');
-							process.exit(1);
-						});
-					});
-				});
-			});
+			waw.fetch(folder, waw.argv[1], () => {
+				console.log('Module has been created');
+				process.exit(1);
+			}, waw.argv.length > 2 ? waw.argv[2] : 'master');
 		}else{
 			fs.mkdirSync(folder, { recursive: true });
 			fs.writeFileSync(folder+'/index.js', `module.exports = function(waw) {\n\t// add your router code\n};`, 'utf8');
 			let data = `{\n\t"name": "CNAME",\n\t"router": "index.js",\n\t"dependencies": {}\n}`;
-			data = data.split('CNAME').join(params.new_part.name.toString().charAt(0).toUpperCase() + params.new_part.name.toString().substr(1).toLowerCase());
-			data = data.split('NAME').join(params.new_part.name.toLowerCase());
+			data = data.split('CNAME').join(waw.new_module.name.toString().charAt(0).toUpperCase() + waw.new_module.name.toString().substr(1).toLowerCase());
+			data = data.split('NAME').join(waw.new_module.name.toLowerCase());
 			fs.writeFileSync(folder+'/part.json', data, 'utf8');
 			console.log('Module has been created');
 			process.exit(1);
 		}
 	};
-	module.exports.add = new_part;
-	module.exports.a = new_part;
+	module.exports.add = new_module;
+	module.exports.a = new_module;
 /*
 *	Version Management
 */
-	const version = function(params){
+	const version = function(waw){
 		let logs = '';
-		if (fs.existsSync(params.waw_root+'/package.json')) {
+		if (fs.existsSync(waw.waw_root+'/package.json')) {
 			try{
-				let config = JSON.parse(fs.readFileSync(params.waw_root+'/package.json'));
+				let config = JSON.parse(fs.readFileSync(waw.waw_root+'/package.json'));
 				if(config.version){
 					logs = 'waw: ' + config.version;
 				}else{
@@ -175,13 +146,13 @@ const git_fetch = function(git, location, repo, branch='master', cb = ()=>{}){
 				process.exit(1);
 			}
 		}
-		if(params.modules.length){
+		if(waw.modules.length){
 			logs += '\nAccesible Modules: ';
-			for (var i = 0; i < params.modules.length; i++) {
+			for (var i = 0; i < waw.modules.length; i++) {
 				if(i){
-					logs += ', '+params.modules[i].name;
+					logs += ', '+waw.modules[i].name;
 				}else{
-					logs += params.modules[i].name;
+					logs += waw.modules[i].name;
 				}
 			}
 
@@ -196,35 +167,35 @@ const git_fetch = function(git, location, repo, branch='master', cb = ()=>{}){
 /*
 *	Git Management
 */
-	const generate = function(params){
-		if(!params.argv.length){
+	const generate = function(waw){
+		if(!waw.argv.length){
 			console.log('Please provide module name');
 			process.exit(1);
 		}
-		let name = params.argv.shift();
-		if(params._parts[name.toLowerCase()]){
+		let name = waw.argv.shift();
+		if(waw._modules[name.toLowerCase()]){
 			console.log('This module already exists.');
 			process.exit(1);
 		}
-		const global_part = params.origin_argv[1].toLowerCase() == 'global' || params.origin_argv[1].toLowerCase() == 'pg';
-		if(global_part && !params.argv.length){
+		const global_module = waw.origin_argv[1].toLowerCase() == 'global' || waw.origin_argv[1].toLowerCase() == 'pg';
+		if(global_module && !waw.argv.length){
 			console.log('To install global module you has to provide repo link.');
 			process.exit(1);
 		}
 		let repo_link;
-		if(params.argv.length){
-			repo_link = params.argv.shift();
+		if(waw.argv.length){
+			repo_link = waw.argv.shift();
 		}
-		let folder = (global_part&&params.waw_root||process.cwd())+'/server/'+name;
+		let folder = (global_module&&waw.waw_root||process.cwd())+'/server/'+name;
 		fs.mkdirSync(folder, { recursive: true });
 		if(repo_link){
-			let repo = params.git(folder);
+			let repo = waw.git(folder);
 			repo.init(function(){
 				repo.addRemote('origin', repo_link, function(err){
 					repo.fetch('--all', function(err){
 						let branch = 'master';
-						if(params.argv.length){
-							branch = params.argv.shift();
+						if(waw.argv.length){
+							branch = waw.argv.shift();
 						}
 						repo.reset('origin/'+branch, err=>{
 							console.log('Module has been created');
@@ -243,112 +214,63 @@ const git_fetch = function(git, location, repo, branch='master', cb = ()=>{}){
 			process.exit(1);
 		}
 	}
-	const parts_renew = function(params){
-		let parts = getDirectories(params.waw_root+'/server');
+	const modules_renew = function(waw){
 		let counter = 0;
-		for (var i = 0; i < parts.length; i++) {
-			if (fs.existsSync(parts[i]+'/part.json')) {
-				let config = JSON.parse(fs.readFileSync(parts[i]+'/part.json'));
-				if(config.git && config.git.repo){
-					counter++;
-					git_fetch(params.git, parts[i], config.git.repo, config.git.branch||'master', ()=>{
-						if(--counter === 0){
-							console.log('All global modules has been updated');
-							process.exit(1);
-						}
-					});
+		for (var i = 0; i < waw.modules.length; i++) {
+			if(!waw.modules[i].git || !waw.modules[i].git.repo) continue;
+			counter++;
+			waw.fetch(waw.modules[i], waw.modules[i].git.repo, ()=>{
+				if (--counter === 0) {
+					console.log('All global modules has been updated');
+					process.exit(1);
 				}
-			}
+			}, waw.modules[i].git.branch || 'master');
 		}
 		if(!counter){
 			console.log('All global modules has been updated');
 			process.exit(1);
 		}
 	}
-	const part = function(params){
-		if(!params.argv.length){
-			console.log('Please provide git command');
-			process.exit(1);
-		}
-		let command = params.argv.shift().toLowerCase();
-		if(command == 'new' || command == 'global'){
-			return generate(params);
-		}
-		if(!params.argv.length){
-			console.log('Please provide module name');
-			process.exit(1);
-		}
-		let part = params.argv.shift();
-		if(!params._parts[part]){
-			console.log("There is no such module");
-			process.exit(1);
-		}
-		if(!params._parts[part].git || !params._parts[part].git.repo){
-			console.log("Module don't have git config");
-			process.exit(1);
-		}
-		switch(command){
-			case 'renew':
-				return parts_renew(params);
-			case 'fetch':
-				let repo = params.git(params._parts[part].__root);
-				repo.init(function(){
-					repo.addRemote('origin', params._parts[part].git.repo, function(err){
-						repo.fetch('--all', function(err){
-							repo.reset('origin/'+(params._parts[part].git.branch||'master'), ()=>{
-								console.log('Module has been fetched');
-								process.exit(1);
-							});
-						});
-					});
-				});
-				return false;
-			default:
-				console.log('Please provide correct git command. Type `waw module help`');
-				process.exit(1);
-		}
-	}
-	module.exports.part = part;
-	module.exports.renew = parts_renew;
+	module.exports.renew = modules_renew;
 	module.exports.add = generate;
 	module.exports.a = generate;
 /*
 *	PM2 management
 */
 	let pm2;
-	const install_pm2 = function(params, callback){
+	const install_pm2 = function(waw, callback){
 		if(pm2) return callback();
-		if (!fs.existsSync(params.waw_root+'/node_modules/pm2')) {
-			return params.npmi({
+		if (!fs.existsSync(waw.waw_root+'/node_modules/pm2')) {
+			return waw.npmi({
 				name: 'pm2',
 				version: 'latest',
-				path: params.waw_root,
+				path: waw.waw_root,
 				forceInstall: true,
 				npmLoad: {
 					loglevel: 'silent'
 				}
 			}, function(){
-				start(params);
+				start(waw);
 			});
 		}
-		if(!pm2) pm2 = require(params.waw_root+'/node_modules/pm2');
+		if(!pm2) pm2 = require(waw.waw_root+'/node_modules/pm2');
 		pm2.connect(function(err) {
 			if (err) {
 				console.error(err);
 				process.exit(2);
 			}
-			if(!params.config.pm2) params.config.pm2={};
+			if(!waw.config.pm2) waw.config.pm2={};
 			callback();
 		});
 	}
-	const start = function(params){
-		install_pm2(params, function(){
+	const start = function(waw){
+		install_pm2(waw, function(){
 			pm2.start({
-				name: params.config.name||process.cwd(),
-				script: params.waw_root+'/app.js',
-				exec_mode: params.config.pm2.exec_mode||'fork', //default fork
-				instances: params.config.pm2.instances||1,
-				max_memory_restart: params.config.pm2.memory||'800M'
+				name: waw.config.name||process.cwd(),
+				script: waw.waw_root+'/app.js',
+				exec_mode: waw.config.pm2.exec_mode||'fork', //default fork
+				instances: waw.config.pm2.instances||1,
+				max_memory_restart: waw.config.pm2.memory||'800M'
 			}, function(err, apps) {
 				pm2.disconnect();
 				process.exit(2);
@@ -356,18 +278,18 @@ const git_fetch = function(git, location, repo, branch='master', cb = ()=>{}){
 		});
 	}
 	module.exports.start = start;
-	const stop = function(params){
-		install_pm2(params, function(){
-			pm2.delete(params.config.name||process.cwd(), function(err, apps) {
+	const stop = function(waw){
+		install_pm2(waw, function(){
+			pm2.delete(waw.config.name||process.cwd(), function(err, apps) {
 				pm2.disconnect();
 				process.exit(2);
 			});
 		});
 	}
 	module.exports.stop = stop;
-	const restart = function(params){
-		install_pm2(params, function(){
-			pm2.restart(params.config.name||process.cwd());
+	const restart = function(waw){
+		install_pm2(waw, function(){
+			pm2.restart(waw.config.name||process.cwd());
 		});
 	}
 	module.exports.restart = restart;
