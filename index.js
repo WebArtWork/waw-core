5f71e461e81ffa0ca59a7e616734f980ba860af1
Honchar Denys <crackeraki@gmail.com>
2022-01-27 02:06:02 +0200

    added http client

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { exec } = require('child_process');
+const https = require('https');
 const fs = require('fs');
+const { Server } = require('http');
 module.exports = function(waw){
 	/* General use */
 		waw.parallel = function(arr, callback){
@@ -81,6 +83,38 @@ module.exports = function(waw){
 			exec(command, (err, stdout, stderr) => {
 				cb({err, stdout, stderr});
 			});
+		}
+	/* Http Management */
+		waw.http = function(hostname, port = 443){
+			const post = function(method){
+				return function (path, body, callback) {
+					const data = new TextEncoder().encode(JSON.stringify(body));
+					const req = https.request({
+						hostname, port, path, method, headers: {
+							'Content-Type': 'application/json',
+							'Content-Length': data.length
+						}
+					}, res => {
+						res.on('data', callback);
+					});
+					req.on('error', error => { });
+					req.write(data);
+					req.end();
+				}
+			}
+			return {
+				get: function(path, callback){
+					const req = https.request({ hostname, port, path, method: 'GET' }, res => {
+						res.on('data', callback);
+					});
+					req.on('error', error => {});
+					req.end();
+				},
+				post: post('POST'),
+				put: post('PUT'),
+				patch: post('PATCH'),
+				delete: post('DELETE')
+			};
 		}
 	/* Events Management */
 		let events = {};
