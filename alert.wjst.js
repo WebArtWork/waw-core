e4d76d29533b8b0b6920cf88c189ad5811523182
Nika <129855721+Nika-cyber1@users.noreply.github.com>
2023-09-09 12:51:33 +0300

    Update alert.wjst.js

--- alert.wjst.js
+++ alert.wjst.js
@@ -1,9 +1,14 @@
 import Dom from "/api/wjst/dom";
 class Alert {
-	show(message) {
+	show(message, timeout = 2000) {
 		const template = Dom.template('alert', { message });
+		
 		if (template && Dom.exists('alert')) {
-			Dom.replace('alert', template)
+			Dom.replace('alert', template);
+			
+			setTimeout(() => {
+				Dom.clear('alert');
+			}, timeout);
 		} else {
 			alert(message)
 		}
