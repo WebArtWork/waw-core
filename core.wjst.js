58262753939ce191eb5070c9aba71c5b3059bda1
Honchar Denys <crackeraki@gmail.com>
2023-10-01 16:54:10 +0300

    initialize core wjst

---
+++ core.wjst.js
@@ -0,0 +1,31 @@
+class Core {
+	wait(ms) {
+		if (typeof conditionFunc === 'function') {
+			return new Promise((resolve) => {
+				setTimeout(resolve, ms);
+			});
+		} else {
+			console.error(`You have to provide function as first parameter`);
+		}
+	}
+
+	waitUntil(conditionFunc, interval = 1000) {
+		if (typeof conditionFunc === 'function') {
+			return new Promise((resolve) => {
+				const checkCondition = () => {
+					if (conditionFunc()) {
+						resolve();
+					} else {
+						setTimeout(checkCondition, interval);
+					}
+				};
+
+				checkCondition();
+			});
+		} else {
+			console.error(`You have to provide function as first parameter`);
+		}
+	}
+}
+
+export default new Core();
