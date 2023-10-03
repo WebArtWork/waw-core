947f515dbb273e11141667c6f9393b607428309a
Nika <129855721+Nika-cyber1@users.noreply.github.com>
2023-10-03 10:20:47 +0300

    Update spinner.wjst.js

--- spinner.wjst.js
+++ spinner.wjst.js
@@ -1,5 +1,18 @@
 import Dom from "/api/wjst/dom";
 class Spinner {
+  	show(message, timeout = 2000) {
+		const template = Dom.template('alert', { message });
+		
+		if (template && Dom.exists('alert')) {
+			Dom.replace('alert', template);
+			
+			setTimeout(() => {
+				Dom.clear('alert');
+			}, timeout);
+		} else {
+			alert(message)
+		}
+	}
 
 }
 export default new Spinner();
