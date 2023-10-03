359228917e68f2fc23cc848ef8b629ec1d7c0f9c
Nika <129855721+Nika-cyber1@users.noreply.github.com>
2023-10-03 10:55:31 +0300

    Update spinner.wjst.js

--- spinner.wjst.js
+++ spinner.wjst.js
@@ -1,18 +1,40 @@
-import Dom from "/api/wjst/dom";
 class Spinner {
-  	show(message, timeout = 2000) {
-		const template = Dom.template('alert', { message });
-		
-		if (template && Dom.exists('alert')) {
-			Dom.replace('alert', template);
-			
-			setTimeout(() => {
-				Dom.clear('alert');
-			}, timeout);
+	ids = [];
+
+	show(name = 'spinner', obj = {}) {
+		const id = 'spinner_' + Date.now();
+
+		this.ids.push(id);
+
+		const template = Dom.template(name, obj);
+
+		if (template) {
+			const childElement = document.createElement("div");
+
+			childElement.innerHTML = childHtml;
+
+			childElement.id = id;
+
+			parentElement.appendChild();
+
+			Dom.addToBody(childElement);
 		} else {
-			alert(message)
+			Dom.addToBody(`<div id="${id}" style="position: fixed; width: 100%; height: 100%; background: white; left:0; top:0"></div>`);
 		}
+
+		return {
+			close: ()=>{
+				Dom.remove(id);
+			},
+			id
+		};
 	}
 
+	close() {
+		for (const id of this.ids) {
+			Dom.remove(id);
+		}
+
+		this.ids = [];
+	}
 }
-export default new Spinner();
