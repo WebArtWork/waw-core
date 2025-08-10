312921090ac995567973a13287e9727076b13714
Honchar Denys <crackeraki@gmail.com>
2025-08-10 16:22:48 +0300

    try to move into wjst.json

--- runner.js
+++ runner.js
@@ -55,6 +55,24 @@ const css_ngx_list = {
 	"6) Skeleton": "https://github.com/WebArtWork/ngx-cssSkeleton.git",
 };
 
+const css_react_list = {
+	"1) Basic": "https://github.com/WebArtWork/react-css.git",
+	"2) Bootstrap": "https://github.com/WebArtWork/react-cssBootstrap.git",
+	"3) Tailwind": "https://github.com/WebArtWork/react-cssTailwind.git",
+	"4) Foundation": "https://github.com/WebArtWork/react-cssFoundation.git",
+	"5) Bulma": "https://github.com/WebArtWork/react-cssBulma.git",
+	"6) Skeleton": "https://github.com/WebArtWork/react-cssSkeleton.git",
+};
+
+const css_vue_list = {
+	"1) Basic": "https://github.com/WebArtWork/vue-css.git",
+	"2) Bootstrap": "https://github.com/WebArtWork/vue-cssBootstrap.git",
+	"3) Tailwind": "https://github.com/WebArtWork/vue-cssTailwind.git",
+	"4) Foundation": "https://github.com/WebArtWork/vue-cssFoundation.git",
+	"5) Bulma": "https://github.com/WebArtWork/vue-cssBulma.git",
+	"6) Skeleton": "https://github.com/WebArtWork/vue-cssSkeleton.git",
+};
+
 const css_wjst_list = {
 	"1) Basic": "https://github.com/WebArtWork/wjst-css.git",
 	"2) Bootstrap": "https://github.com/WebArtWork/wjst-cssBootstrap.git",
@@ -96,9 +114,26 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 `;
 
+const isAngular = fs.existsSync(path.join(process.cwd(), "angular.json"));
+const isReact = fs.existsSync(path.join(process.cwd(), "react.json"));
+const isVue = fs.existsSync(path.join(process.cwd(), "vue.json"));
+const isWjst =
+	fs.existsSync(path.join(process.cwd(), "template.json")) ||
+	fs.existsSync(path.join(process.cwd(), "wjst.json"));
+
+if (
+	fs.existsSync(path.join(process.cwd(), "template.json")) &&
+	!fs.existsSync(path.join(process.cwd(), "wjst.json"))
+) {
+	fs.copyFileSync(
+		path.join(process.cwd(), "template.json"),
+		path.join(process.cwd(), "wjst.json")
+	);
+}
+
 module.exports.love = function (waw) {
 	console.log("waw Loves you :) ");
-	
+
 	process.exit();
 };
 /*
@@ -230,13 +265,9 @@ module.exports.n = new_project;
  *	Update css folder
  */
 const change_css = function (waw) {
-	const isAngular = fs.existsSync(path.join(process.cwd(), "angular.json"));
-
-	if (
-		!isAngular &&
-		!fs.existsSync(path.join(process.cwd(), "template.json"))
-	) {
+	if (!isAngular && !isReact && !isVue && !isWjst) {
 		console.log("This is not an project with waw css");
+
 		process.exit();
 	}
 
@@ -245,15 +276,25 @@ const change_css = function (waw) {
 	}
 
 	if (!waw.change_css.repo) {
-		const list = isAngular ? css_ngx_list : css_wjst_list;
+		const list = isAngular
+			? css_ngx_list
+			: isReact
+			? css_react_list
+			: isVue
+			? css_vue_list
+			: css_wjst_list;
+
 		let text = "Which framework you want to use?",
 			counter = 0,
 			repos = {};
+
 		for (let key in list) {
 			repos[++counter] = list[key];
 			text += "\n" + key;
 		}
+
 		text += "\nChoose number: ";
+
 		return waw.readline.question(text, function (answer) {
 			if (!answer || !repos[parseInt(answer)]) return change_css(waw);
 			waw.change_css.repo = repos[parseInt(answer)];
@@ -261,10 +302,16 @@ const change_css = function (waw) {
 		});
 	}
 
-	const folder = path.join(process.cwd(), isAngular ? "src" : "css", "scss");
+	const folder = path.join(
+		process.cwd(),
+		isAngular || isReact || isVue ? "src" : "css",
+		"scss"
+	);
 
 	fs.rmSync(folder, { recursive: true });
+
 	fs.mkdirSync(folder, { recursive: true });
+
 	waw.fetch(
 		folder,
 		waw.change_css.repo,
